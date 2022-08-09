73f500323fadef6082cf7e160e1336e35c9a50f9
b1511 <borislava.ph@gmail.com>
2022-08-09 15:53:02 +0200

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -134,34 +134,3 @@ renderVid();
 function displayVid(selector, newContent) {
   document.querySelector(selector).innerHTML += newContent;
 };
-
-/*attempt to make contact form working
-var btn = document.getElementById('btn');
-
-btn.addEventListener('click', function(e){
-    e.preventDefault()
-    var name = document.getElementById('name').value;
-    var email = document.getElementById('email').value;
-    var subject = document.getElementById('subject').value;
-    var msg = document.getElementById('message').value;
-    var body = 'name:' + name + '<br/> email: ' + email + '<br/> message:' + '<br/> subject: ' + subject + msg;
-
-    Email.send({
-        Host : "smtp.gmail.com",
-        Username : "borislava.ph@gmail.com",
-        Password : "xcyzsreqomrrvxuj",
-        To : 'borislava.ph@gmail.com',
-        From : email,
-        Subject : subject,
-        Body : body
-    }).then(
-      message => alert(msg)
-    );
-});
-*/
-
-/* attempt to get image url from local data file and use it for img src
-fetch('http://127.0.0.1:5500/assets/js/data.js')
-  .then(response => response.json())
-  .then(data => console.log(data));
-*/
